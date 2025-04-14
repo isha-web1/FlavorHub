ad7f35eec4c59a74e4ecfab11d05f5d626fb1013
isha-web1 <isha.web1@gmail.com>
2025-04-14 23:49:37 +0600

    category & shared section title component added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../banner/Banner';
+import Category from '../category/Category';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Category/>
         </div>
     );
 };
