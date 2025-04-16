80314bcd3f6fce428a6ea6d6857386dda3c6445a
isha-web1 <isha.web1@gmail.com>
2025-04-16 13:32:16 +0600

    menu cover & menu category added

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../shared/Cover';
+import menuImg from '../../assets/menu/banner3.jpg'
+import PopularMenu from '../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -7,6 +10,8 @@ const Menu = () => {
              <Helmet>
                 <title>FlavorHub | Menu</title>
             </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu/>
         </div>
     );
 };
