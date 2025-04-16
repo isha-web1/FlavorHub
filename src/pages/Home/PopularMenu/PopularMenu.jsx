db3904c6ee568d028dd59fa61692bc12b4226e82
isha-web1 <isha.web1@gmail.com>
2025-04-16 20:45:15 +0600

    use custom hook for load menu data & menu category

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,38 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import SectionTitle from '../../../components/sectionTitle/SectionTitle';
-import MenuItem from '../../shared/MenuItems';
-// import useMenu from '../../../hooks/UseMenu';
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../shared/MenuItems";
+import useMenu from "../../../Hooks/UseMenu";
+
 
 const PopularMenu = () => {
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular');
     
-    const [menu, setMenu] = useState([])
-
-    useEffect(() =>{
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular');
-            setMenu(popularItems)
-        })
-    },[])
-
     return (
-        <section className='mb-12 px-4'>
-           <SectionTitle
-           heading={"From Our Menu"}
-           subHeading={"Popular Items"}
-           >
-            </SectionTitle> 
-            <div className='grid md: grid-cols-2 gap-4'>
+        <section className="mb-12">
+            <SectionTitle
+                heading="From Our Menu"
+                subHeading="Popular Items"
+            ></SectionTitle>
+            <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item => <MenuItem 
-                        key={item._item}
+                    popular.map(item => <MenuItem
+                        key={item._id}
                         item={item}
                     ></MenuItem>)
                 }
             </div>
-            <div className='text-center mt-6 '>
-            <button className='border-b-4 decoration-slate-600'>view full menu</button>
+            <div className="text-center mt-12"> 
+            <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
             </div>
         </section>
     );
