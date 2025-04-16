5a78c17b1d5196383282cdaeea47dc265fe8a389
isha-web1 <isha.web1@gmail.com>
2025-04-16 21:54:44 +0600

    create order page & display data through tabs

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from '../pages/menu/Menu'
+import Order from "../pages/Home/order/Order";
 
 export const router = createBrowserRouter([
     {
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
             {
                 path : 'menu',
                 element : <Menu/>
+            },
+            {
+                path : 'order/:category',
+                element : <Order/>
             }
         ]
     }
